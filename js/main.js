236f3a2fd77c0567833283e52af7256c65eaa2bb
Charlie <Jarvis@Jarviss-MacBook-Pro.local>
2015-10-13 13:52:59 +0100

    One
    Responsive / Customisable / Video Background

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// HTML5 VIDEO PLUGIN
+var vid = document.getElementById("bgvid");
+var pauseButton = document.getElementById("vidpause");
+function vidFade() {
+vid.classList.add("stopfade");
+}
+vid.addEventListener('ended', function() {
+// only functional if "loop" is removed
+vid.pause();
+// to capture IE10
+vidFade();
+});
+pauseButton.addEventListener("click", function() {
+vid.classList.toggle("stopfade");
+if (vid.paused) {
+vid.play();
+pauseButton.innerHTML = '<i class="fa fa-pause"></i>';
+} else {
+vid.pause();
+pauseButton.innerHTML = '<i class="fa fa-play"></i>';
+}
+})
